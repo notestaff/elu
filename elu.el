a25b26b11acef05abc6194276cdbed30ce37b5e1
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2012-04-18 17:20:16 -0400

    elu: Added XEmacs compatibility changes

--- elu.el
+++ elu.el
@@ -285,6 +285,112 @@ Return t if any property was actually removed, nil otherwise."
 			      object)
     (remove-list-of-text-properties start end list-of-properties object)))
 
+(when (featurep 'xemacs)  
+  (defvar bind-enquote nil))
+
+(if (fboundp 'regexp-opt)
+    (defalias 'elu-regexp-opt 'regexp-opt)
+  (defun elu-regexp-opt (strings &optional paren)
+    "Copied from `regexp-opt'."
+    (let ((open (if paren "\\(" "")) (close (if paren "\\)" "")))
+      (concat open (mapconcat 'regexp-quote strings "\\|") close))))
+
+(if (fboundp 'regexp-opt-depth)
+    (defalias 'elu-regexp-opt-depth 'regexp-opt-depth)
+  (defun elu-regexp-opt-depth (regexp)
+    "Return the depth of REGEXP.
+This means the number of non-shy regexp grouping constructs
+\(parenthesized expressions) in REGEXP.  Copied from `regexp-opt-depth'."
+    (save-match-data
+      ;; Hack to signal an error if REGEXP does not have balanced parentheses.
+      (string-match regexp "")
+      ;; Count the number of open parentheses in REGEXP.
+      (let ((count 0) start last)
+	(while (string-match "\\\\(\\(\\?[0-9]*:\\)?" regexp start)
+	  (setq start (match-end 0))	      ; Start of next search.
+	  (when (and (not (match-beginning 1))
+		     (subregexp-context-p regexp (match-beginning 0) last))
+	    ;; It's not a shy group and it's not inside brackets or after
+	    ;; a backslash: it's really a group-open marker.
+	    (setq last start)	    ; Speed up next regexp-opt-re-context-p.
+	    (setq count (1+ count))))
+	count))))
+
+(if (fboundp 'delete-dups)
+    (defalias 'elu-delete-dups 'delete-dups)
+  (defun elu-delete-dups (list)
+    "Destructively remove `equal' duplicates from LIST.
+Store the result in LIST and return it.  LIST must be a proper list.
+Of several `equal' occurrences of an element in LIST, the first
+one is kept.  Copied from `delete-dups' in GNU Emacs."
+    (let ((tail list))
+      (while tail
+	(setcdr tail (delete (car tail) (cdr tail)))
+	(setq tail (cdr tail))))
+    list))
+
+(if (fboundp 'replace-regexp-in-string)
+    (defalias 'elu-replace-regexp-in-string 'replace-regexp-in-string)
+(defun replace-regexp-in-string (regexp rep string &optional
+					fixedcase literal subexp start)
+  "Replace all matches for REGEXP with REP in STRING.
+
+Return a new string containing the replacements.
+
+Optional arguments FIXEDCASE, LITERAL and SUBEXP are like the
+arguments with the same names of function `replace-match'.  If START
+is non-nil, start replacements at that index in STRING.
+
+REP is either a string used as the NEWTEXT arg of `replace-match' or a
+function.  If it is a function, it is called with the actual text of each
+match, and its value is used as the replacement text.  When REP is called,
+the match data are the result of matching REGEXP against a substring
+of STRING.
+
+To replace only the first match (if any), make REGEXP match up to \\'
+and replace a sub-expression, e.g.
+  (replace-regexp-in-string \"\\\\(foo\\\\).*\\\\'\" \"bar\" \" foo foo\" nil nil 1)
+    => \" bar foo\"
+
+Copied from `replace-regexp-in-string' in GNU Emacs.
+"
+
+  ;; To avoid excessive consing from multiple matches in long strings,
+  ;; don't just call `replace-match' continually.  Walk down the
+  ;; string looking for matches of REGEXP and building up a (reversed)
+  ;; list MATCHES.  This comprises segments of STRING which weren't
+  ;; matched interspersed with replacements for segments that were.
+  ;; [For a `large' number of replacements it's more efficient to
+  ;; operate in a temporary buffer; we can't tell from the function's
+  ;; args whether to choose the buffer-based implementation, though it
+  ;; might be reasonable to do so for long enough STRING.]
+  (let ((l (length string))
+	(start (or start 0))
+	matches str mb me)
+    (save-match-data
+      (while (and (< start l) (string-match regexp string start))
+	(setq mb (match-beginning 0)
+	      me (match-end 0))
+	;; If we matched the empty string, make sure we advance by one char
+	(when (= me mb) (setq me (min l (1+ mb))))
+	;; Generate a replacement for the matched substring.
+	;; Operate only on the substring to minimize string consing.
+	;; Set up match data for the substring for replacement;
+	;; presumably this is likely to be faster than munging the
+	;; match data directly in Lisp.
+	(string-match regexp (setq str (substring string mb me)))
+	(setq matches
+	      (cons (replace-match (if (stringp rep)
+				       rep
+				     (funcall rep (match-string 0 str)))
+				   fixedcase literal str subexp)
+		    (cons (substring string start mb) ; unmatched prefix
+			  matches)))
+	(setq start me))
+      ;; Reconstruct a string from the pieces.
+      (setq matches (cons (substring string start l) matches)) ; leftover
+      (apply #'concat (nreverse matches))))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; Section: Debugging helpers
