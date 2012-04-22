88808ad31e95185d8c79ecc89ea989c7bff564d8
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2012-04-22 14:34:22 -0400

    rxx: Copied regex-opt implementation from GNU Emacs for xemacs compatibility.

--- elu.el
+++ elu.el
@@ -290,10 +290,194 @@ Return t if any property was actually removed, nil otherwise."
 
 (if (fboundp 'regexp-opt)
     (defalias 'elu-regexp-opt 'regexp-opt)
-  (defun elu-regexp-opt (strings &optional paren)
-    "Copied from `regexp-opt'."
-    (let ((open (if paren "\\(" "")) (close (if paren "\\)" "")))
-      (concat open (mapconcat 'regexp-quote strings "\\|") close))))
+
+(defun elu-regexp-opt (strings &optional paren)
+  "Return a regexp to match a string in the list STRINGS.
+Each string should be unique in STRINGS and should not contain any regexps,
+quoted or not.  If optional PAREN is non-nil, ensure that the returned regexp
+is enclosed by at least one regexp grouping construct.
+The returned regexp is typically more efficient than the equivalent regexp:
+
+ (let ((open (if PAREN \"\\\\(\" \"\")) (close (if PAREN \"\\\\)\" \"\")))
+   (concat open (mapconcat 'regexp-quote STRINGS \"\\\\|\") close))
+
+If PAREN is `words', then the resulting regexp is additionally surrounded
+by \\=\\< and \\>.
+If PAREN is `symbols', then the resulting regexp is additionally surrounded
+by \\=\\_< and \\_>.
+
+Copied from `regexp-opt' in GNU Emacs."
+  (save-match-data
+    ;; Recurse on the sorted list.
+    (let* ((max-lisp-eval-depth 10000)
+	   (max-specpdl-size 10000)
+	   (completion-ignore-case nil)
+	   (completion-regexp-list nil)
+	   (open (cond ((stringp paren) paren) (paren "\\(")))
+	   (sorted-strings (elu-delete-dups
+			    (sort (copy-sequence strings) 'string-lessp)))
+	   (re (elu-regexp-opt-group sorted-strings (or open t) (not open))))
+      (cond ((eq paren 'words)
+	     (concat "\\<" re "\\>"))
+	    ((eq paren 'symbols)
+	     (concat "\\_<" re "\\_>"))
+	    (t re))))))
+
+
+(if (fboundp 'regexp-opt-group)
+    (defalias 'elu-regexp-opt-group 'regexp-opt-group)
+(defun elu-regexp-opt-group (strings &optional paren lax)
+  "Return a regexp to match a string in the sorted list STRINGS.
+If PAREN non-nil, output regexp parentheses around returned regexp.
+If LAX non-nil, don't output parentheses if it doesn't require them.
+Merges keywords to avoid backtracking in Emacs's regexp matcher."
+  ;; The basic idea is to find the shortest common prefix or suffix, remove it
+  ;; and recurse.  If there is no prefix, we divide the list into two so that
+  ;; \(at least) one half will have at least a one-character common prefix.
+
+  ;; Also we delay the addition of grouping parenthesis as long as possible
+  ;; until we're sure we need them, and try to remove one-character sequences
+  ;; so we can use character sets rather than grouping parenthesis.
+  (let* ((open-group (cond ((stringp paren) paren) (paren "\\(?:") (t "")))
+	 (close-group (if paren "\\)" ""))
+	 (open-charset (if lax "" open-group))
+	 (close-charset (if lax "" close-group)))
+    (cond
+     ;;
+     ;; If there are no strings, just return the empty string.
+     ((= (length strings) 0)
+      "")
+     ;;
+     ;; If there is only one string, just return it.
+     ((= (length strings) 1)
+      (if (= (length (car strings)) 1)
+	  (concat open-charset (regexp-quote (car strings)) close-charset)
+	(concat open-group (regexp-quote (car strings)) close-group)))
+     ;;
+     ;; If there is an empty string, remove it and recurse on the rest.
+     ((= (length (car strings)) 0)
+      (concat open-charset
+	      (elu-regexp-opt-group (cdr strings) t t) "?"
+	      close-charset))
+     ;;
+     ;; If there are several one-char strings, use charsets
+     ((and (= (length (car strings)) 1)
+	   (let ((strs (cdr strings)))
+	     (while (and strs (/= (length (car strs)) 1))
+	       (pop strs))
+	     strs))
+      (let (letters rest)
+	;; Collect one-char strings
+	(dolist (s strings)
+	  (if (= (length s) 1) (push (string-to-char s) letters) (push s rest)))
+
+	(if rest
+	    ;; several one-char strings: take them and recurse
+	    ;; on the rest (first so as to match the longest).
+	    (concat open-group
+		    (elu-regexp-opt-group (nreverse rest))
+		    "\\|" (elu-regexp-opt-charset letters)
+		    close-group)
+	  ;; all are one-char strings: just return a character set.
+	  (concat open-charset
+		  (elu-regexp-opt-charset letters)
+		  close-charset))))
+     ;;
+     ;; We have a list of different length strings.
+     (t
+      (let ((prefix (try-completion "" strings)))
+	(if (> (length prefix) 0)
+	    ;; common prefix: take it and recurse on the suffixes.
+	    (let* ((n (length prefix))
+		   (suffixes (mapcar (lambda (s) (substring s n)) strings)))
+	      (concat open-group
+		      (regexp-quote prefix)
+		      (elu-regexp-opt-group suffixes t t)
+		      close-group))
+
+	  (let* ((sgnirts (mapcar (lambda (s)
+				    (concat (nreverse (string-to-list s))))
+				  strings))
+		 (xiffus (try-completion "" sgnirts)))
+	    (if (> (length xiffus) 0)
+		;; common suffix: take it and recurse on the prefixes.
+		(let* ((n (- (length xiffus)))
+		       (prefixes
+			;; Sorting is necessary in cases such as ("ad" "d").
+			(sort (mapcar (lambda (s) (substring s 0 n)) strings)
+			      'string-lessp)))
+		  (concat open-group
+			  (elu-regexp-opt-group prefixes t t)
+			  (regexp-quote
+			   (concat (nreverse (string-to-list xiffus))))
+			  close-group))
+
+	      ;; Otherwise, divide the list into those that start with a
+	      ;; particular letter and those that do not, and recurse on them.
+	      (let* ((char (substring-no-properties (car strings) 0 1))
+		     (half1 (all-completions char strings))
+		     (half2 (nthcdr (length half1) strings)))
+		(concat open-group
+			(elu-regexp-opt-group half1)
+			"\\|" (regexp-opt-group half2)
+			close-group)))))))))))
+
+(if (fboundp 'regexp-opt-charset)
+    (defalias 'elu-regexp-opt-charset 'regexp-opt-charset)
+(defun elu-regexp-opt-charset (chars)
+  "Return a regexp to match a character in CHARS."
+  ;; The basic idea is to find character ranges.  Also we take care in the
+  ;; position of character set meta characters in the character set regexp.
+  ;;
+  (let* ((charmap (make-char-table 'case-table))
+	 (start -1) (end -2)
+	 (charset "")
+	 (bracket "") (dash "") (caret ""))
+    ;;
+    ;; Make a character map but extract character set meta characters.
+    (dolist (char chars)
+      (case char
+	(?\]
+	 (setq bracket "]"))
+	(?^
+	 (setq caret "^"))
+	(?-
+	 (setq dash "-"))
+	(otherwise
+	 (aset charmap char t))))
+    ;;
+    ;; Make a character set from the map using ranges where applicable.
+    (map-char-table
+     (lambda (c v)
+       (when v
+	 (if (consp c)
+	     (if (= (1- (car c)) end) (setq end (cdr c))
+	       (if (> end (+ start 2))
+		   (setq charset (format "%s%c-%c" charset start end))
+		 (while (>= end start)
+		   (setq charset (format "%s%c" charset start))
+		   (incf start)))
+	       (setq start (car c) end (cdr c)))
+	   (if (= (1- c) end) (setq end c)
+	     (if (> end (+ start 2))
+	       (setq charset (format "%s%c-%c" charset start end))
+	     (while (>= end start)
+	       (setq charset (format "%s%c" charset start))
+	       (incf start)))
+	     (setq start c end c)))))
+     charmap)
+    (when (>= end start)
+      (if (> end (+ start 2))
+	  (setq charset (format "%s%c-%c" charset start end))
+	(while (>= end start)
+	  (setq charset (format "%s%c" charset start))
+	  (incf start))))
+    ;;
+    ;; Make sure a caret is not first and a dash is first or last.
+    (if (and (string-equal charset "") (string-equal bracket ""))
+	(concat "[" dash caret "]")
+      (concat "[" bracket charset caret dash "]")))))
+
 
 (if (fboundp 'subregexp-context-p)
     (defalias 'elu-subregexp-context-p 'subregexp-context-p)
